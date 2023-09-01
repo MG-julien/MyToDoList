95ad4287df667ec1ab1a518a4490bf524175b050
Genty Julien <M.G.dev.management@gmail.com>
2023-09-01 19:13:07 +0200

    form config + event add list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./style/index.scss";
+
+const addTaskBtn = document.querySelector("[name='submitTask']");
+
+const listTaskContainer = document.querySelector(".listTaskContainer");
+const listTaskContainer__ul = document.createElement("ul");
+listTaskContainer.appendChild(listTaskContainer__ul);
+
+addTaskBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const taskValueContainer = document.querySelector("[name='addTask']");
+  const taskValue = taskValueContainer.value;
+
+  const addTaskConstructor = `
+        <li>${taskValue}</li>
+    `;
+  listTaskContainer__ul.innerHTML += addTaskConstructor;
+  if (event) {
+    taskValueContainer.value = "";
+  }
+});
