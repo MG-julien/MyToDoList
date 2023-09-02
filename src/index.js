60bea038725ab77653e78a99dff737e5a3148a3c
Genty Julien <M.G.dev.management@gmail.com>
2023-09-02 21:52:15 +0200

    HTML configuration terminée

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import "./style/index.scss";
 
 const addTaskBtn = document.querySelector("[name='submitTask']");
-
-const listTaskContainer = document.querySelector(".listTaskContainer");
-const listTaskContainer__ul = document.createElement("ul");
-listTaskContainer.appendChild(listTaskContainer__ul);
+const listTaskContainer = document.querySelector(".listTaskContainer__ul");
 
 addTaskBtn.addEventListener("click", (event) => {
-  event.preventDefault();
+    event.preventDefault();
+    const taskValueContainer = document.querySelector("[name='addTask']");
 
-  const taskValueContainer = document.querySelector("[name='addTask']");
-  const taskValue = taskValueContainer.value;
+    const taskValue = taskValueContainer.value;
+    const addTaskConstructor = `
+    <li class="">
+      
+      <input type="checkbox" name="jkjk"/>
+    </li>
+  `;
 
-  const addTaskConstructor = `
-        <li>${taskValue}</li>
-    `;
-  listTaskContainer__ul.innerHTML += addTaskConstructor;
-  if (event) {
-    taskValueContainer.value = "";
-  }
+    listTaskContainer.innerHTML += addTaskConstructor;
+
+    if (event) {
+        taskValueContainer.value = "";
+    }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,50 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images/",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html"),
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-    }),
-  ],
-  devtool: "source-map",
-  mode: "development",
-  devServer: {
-    static: path.resolve(__dirname, "./dist"),
-    open: true,
-    watchFiles: ["./src/**"],
-    port: 4000,
-    hot: true,
-  },
+    devtool: "source-map",
+    mode: "development",
+    devServer: {
+        static: path.resolve(__dirname, "./dist"),
+        open: true,
+        watchFiles: ["./src/**"],
+        port: 4000,
+        hot: true,
+    },
 };
